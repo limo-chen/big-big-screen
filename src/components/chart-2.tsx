6bc23bed7efdc76a7bd4eceed93dfcd0026f3704
Calista <2320035651@qq.com>
2023-04-18 16:16:05 +0800

    添加渐变

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -19,15 +19,15 @@ export const Chart2 = () => {
           axisTick: { show: false },
           type: "category",
           data: [
-            "城关区公安局",
-            "七里河区公安局",
-            "西固区公安局",
-            "安宁区公安局",
-            "红古区公安局",
-            "永登县公安局",
-            "皋兰县公安局",
-            "榆中县公安局",
-            "新区公安局",
+            "扶风区",
+            "西安区",
+            "中余区",
+            "机场区",
+            "新开区",
+            "临安区",
+            "古新区",
+            "都城区",
+            "蓝星新区",
           ],
           axisLabel: {
             formatter(val) {
@@ -40,11 +40,39 @@ export const Chart2 = () => {
             name: "2011年",
             type: "bar",
             data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                  {
+                    offset: 0,
+                    color: "#2034f9",
+                  },
+                  {
+                    offset: 1,
+                    color: "#04a1ff",
+                  },
+                ]),
+              },
+            },
           },
           {
             name: "2012年",
             type: "bar",
             data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                  {
+                    offset: 0,
+                    color: "#b92ae8",
+                  },
+                  {
+                    offset: 1,
+                    color: "#6773e7",
+                  },
+                ]),
+              },
+            },
           },
         ],
       })
@@ -52,13 +80,14 @@ export const Chart2 = () => {
   }, []);
 
   return (
-    <div className="bordered 破获排名">
-      <h2>案件破获排名</h2>
+    <div className="bordered 疫情实时动态">
+      <h2>疫情实时动态</h2>
 
       <div ref={divRef} className="chart" />
       <div className="legend">
-        <span className="first" /> 破案排名1
-        <span className="second" /> 破案排名1
+        <span className="first" />
+        无症状患者
+        <span className="second" /> 新增确诊患者
       </div>
     </div>
   );
